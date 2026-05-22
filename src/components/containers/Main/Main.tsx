05a26a69d141a0b7aa1eb3f833ee746809e922f6
Christelle Souka <soukachristelle@gmail.com>
2026-05-22 14:51:48 +0200

    feat: add Projects component in content

---
+++ src/components/containers/Main/Main.tsx
@@ -0,0 +1,13 @@
+import Home from "../../children/Main/Home";
+import Projects from "../../children/Projects/Projects";
+
+function Main() {
+  return (
+    <div className="home">
+      <Home/>
+      <Projects/>
+    </div>
+  );
+}
+
+export default Main;
