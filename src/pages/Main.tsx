905517019d61c63f3c6fe8cf9414c8bb1ef0cb67
Christelle Souka <soukachristelle@gmail.com>
2026-06-11 22:42:40 +0200

    style: add background image

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,6 +4,17 @@ import Projects from "./Projects";
 function Main() {
   return (
     <main className="w-full flex flex-col font-nunito">
+      <img
+        src="/assets/app/yellow-background.png"
+        alt="yellow background"
+        className="absolute right-0"
+      />
+
+      <img
+        src="/assets/app/image.png"
+        alt="portrait"
+        className="absolute right-0"
+      />
       <Presentation />
       <Projects />
     </main>
