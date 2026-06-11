bb98a736d5357d3ddf6af18b9991973f3da229d7
Christelle Souka <soukachristelle@gmail.com>
2026-06-11 22:31:30 +0200

    style: add color on hover on icons

--- src/components/containers/Footer.tsx
+++ src/components/containers/Footer.tsx
@@ -4,14 +4,14 @@ import { IoIosMail } from "react-icons/io";
 
 function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center py-20">
-      <div className="flex items-center p-4">
-        <AiFillInstagram size={70} className="p-4" />
-        <FaLinkedin size={70} className="p-4" />
-        <IoIosMail size={70} className="p-4" />
+    <footer className="flex flex-col items-center justify-center pb-6 shrink-0 bg-[#F9FAFF] w-full">
+      <div className="flex items-center p-2">
+        <AiFillInstagram size={50} className="p-2 cursor-pointer hover:text-[#FDC435] transition-colors" />
+        <FaLinkedin size={50} className="p-2 cursor-pointer hover:text-[#FDC435] transition-colors" />
+        <IoIosMail size={50} className="p-2 cursor-pointer hover:text-[#FDC435] transition-colors" />
       </div>
 
-      <span className="text-[#828282] font-nunito">
+      <span className="text-[#828282] font-nunito text-[14px]">
         Madelyn Torff 2026
       </span>
     </footer>
